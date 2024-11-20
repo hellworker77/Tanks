c21811dbdf616fb18c9665afee8dc2e2c580d902
Ilya <iljapuskov@gmail.com>
2024-11-20 20:54:43 +0100

    feat: logger updated

--- src/Logger/Logger.cpp
+++ src/Logger/Logger.cpp
@@ -9,10 +9,60 @@ void Logger::subscribe(Logger::Subscriber subscriber) {
     m_subscriber = std::move(subscriber);
 }
 
-void Logger::print(const std::string &message) {
-    print(message, false);
+void Logger::setLogLevel(LogLevel logLevel) {
+    m_logLevel = logLevel;
 }
 
-void Logger::error(const std::string &message) {
-    print(message, true);
+
+void Logger::print(const std::string &message, bool logging) {
+    std::string log;
+    print(message, "[PRINT]", m_subscriber.m_messageColor, log);
+
+    if(m_logLevel <= ALL && logging){
+        fileLog(log);
+    }
+
 }
+
+void Logger::warning(const std::string &message, bool logging) {
+    std::string log;
+    print(message, "[WARNING]", WARNING_COLOR, log);
+    if(m_logLevel <= WARNING && logging){
+        fileLog(log);
+    }
+}
+
+void Logger::info(const std::string &message, bool logging) {
+    std::string log;
+    print(message, "[INFO]", INFO_COLOR, log);
+    if(m_logLevel <= INFO && logging){
+        fileLog(log);
+    }
+}
+
+void Logger::fatal(const std::string &message, bool logging) {
+    std::string log;
+    print(message, "[FATAL]", FATAL_COLOR, log);
+    if(m_logLevel <= FATAL && logging){
+        fileLog(log);
+    }
+}
+
+void Logger::success(const std::string &message, bool logging) {
+    std::string log;
+    print(message, "[SUCCESS]", SUCCESS_COLOR, log);
+    if(m_logLevel <= SUCCESS && logging){
+        fileLog(log);
+    }
+}
+
+void Logger::message(const std::string &message, bool logging) {
+    std::string log;
+    print(message, "[MESSAGE]", DEFAULT_COLOR, log);
+    if(m_logLevel <= MESSAGE && logging){
+        fileLog(log);
+    }
+}
+
+
+
--- src/Logger/Logger.h
+++ src/Logger/Logger.h
@@ -1,7 +1,7 @@
 //
 // Created by iljap on 11/17/2024.
 //
-
+#pragma once
 #ifndef BATTLECITY_LOGGER_H
 #define BATTLECITY_LOGGER_H
 
@@ -12,64 +12,137 @@
 #include <windows.h>
 #include <iostream>
 #include <utility>
+#include <fstream>
+#include <filesystem>
 
-#define ERROR_COLOR 0xC
+#define WARNING_COLOR 0x4
+#define INFO_COLOR 0x9
+#define SUCCESS_COLOR 0xA
+#define FATAL_COLOR 0xC
 #define DEFAULT_COLOR 0xF
+
+enum LogLevel {
+    ALL,
+    MESSAGE,
+    SUCCESS,
+    INFO,
+    WARNING,
+    FATAL
+};
 
 class Logger {
 public:
+    struct Subscriber {
+        friend Logger;
+    public:
+        Subscriber() = default;
+
+        Subscriber(const std::string &target,
+                   char targetColor,
+                   char messageColor,
+                   bool displayTime) {
+
+            m_target = target;
+            m_targetColor = targetColor;
+            m_messageColor = messageColor;
+            m_displayTime = displayTime;
+        }
+
+    private:
+
+        std::string m_target = "ENGINE";
+        char m_targetColor = DEFAULT_COLOR;
+        char m_messageColor = DEFAULT_COLOR;
+        bool m_displayTime = DEFAULT_COLOR;
+    };
+public:
+    void print(const std::string &message, bool logging = true);
+
+    void warning(const std::string &message, bool logging = true);
+
+    void info(const std::string &message, bool logging = true);
+
+    void fatal(const std::string &message, bool logging = true);
+
+    void success(const std::string &message, bool logging = true);
+
+    void message(const std::string &message, bool logging = true);
+
+    void subscribe(Subscriber subscriber);
+
+    void setLogLevel(LogLevel logLevel);
+
+    Logger(const Logger &) = delete;
+
+    Logger &operator=(const Logger &) = delete;
+
+    static Logger &getInstance() {
+        static Logger m_instance;
+        return m_instance;
+    }
+
+private:
     Logger() = default;
+
     ~Logger() = default;
 
-    struct Subscriber{
-        std::string target = "ENGINE";
-
-        char timeColor;
-        char targetColor;
-        char messageColor;
-
-        bool displayTime;
-    };
-
-public:
-    virtual void print(const std::string& message);
-    virtual void error(const std::string& message);
-    virtual void subscribe(Subscriber subscriber);
-private:
     HANDLE m_hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
     Subscriber m_subscriber;
-    void print(const std::string& message, bool isError){
-        char error_color = 12;
+    LogLevel m_logLevel = WARNING;
 
-        if (m_subscriber.displayTime) {
-            std::time_t t = std::time(nullptr);
-            std::tm *time_info = std::localtime(&t);
-            std::ostringstream oss;
+    void print(const std::string &message, const char* suffix, char color, std::string& formated) {
 
-            oss << "["
-                << std::setw(2) << std::setfill('0') << (time_info->tm_mon + 1) << "."
-                << std::setw(2) << std::setfill('0') << time_info->tm_mday << " "
-                << std::setw(2) << std::setfill('0') << time_info->tm_hour << ":"
-                << std::setw(2) << std::setfill('0') << time_info->tm_min << "]";
+        std::time_t t = std::time(nullptr);
+        std::tm *time_info = std::localtime(&t);
+        std::ostringstream oss;
+
+        oss << "["
+            << std::setw(2) << std::setfill('0') << (time_info->tm_mon + 1) << "."
+            << std::setw(2) << std::setfill('0') << time_info->tm_mday << " "
+            << std::setw(2) << std::setfill('0') << time_info->tm_hour << ":"
+            << std::setw(2) << std::setfill('0') << time_info->tm_min << "]";
 
 
-            std::string current_time = oss.str();
-            SetConsoleTextAttribute(m_hConsole, m_subscriber.timeColor);
-            std::cout << oss.str() << " ";
+        std::string current_time = oss.str();
+
+
+        if (m_subscriber.m_displayTime) {
+            SetConsoleTextAttribute(m_hConsole, DEFAULT_COLOR);
+            std::cout << current_time << " ";
         }
 
-        SetConsoleTextAttribute(m_hConsole, m_subscriber.targetColor);
-        std::cout << m_subscriber.target << ": ";
+        SetConsoleTextAttribute(m_hConsole, m_subscriber.m_targetColor);
+        std::cout << m_subscriber.m_target << " ";
 
-        if (isError) {
-            SetConsoleTextAttribute(m_hConsole, ERROR_COLOR);
-        } else {
-            SetConsoleTextAttribute(m_hConsole, m_subscriber.messageColor);
-        }
+        SetConsoleTextAttribute(m_hConsole, color);
 
-        std::cout << message << std::endl;
+        std::cout << suffix <<": " << message << std::endl;
 
         SetConsoleTextAttribute(m_hConsole, DEFAULT_COLOR);
+
+        formated = current_time + " "+ m_subscriber.m_target + " " + suffix + ": " + message;
+    }
+
+    void fileLog(const std::string& log){
+        try {
+            std::filesystem::path logDir = "logs";
+            if (!std::filesystem::exists(logDir)) {
+                std::filesystem::create_directory(logDir);
+                message("Created logs directory...", false);
+            }
+
+            std::filesystem::path logFile = logDir / "runtimelogs.txt";
+
+            std::ofstream file(logFile, std::ios::app);
+            if (!file) {
+                throw std::ios_base::failure("Unable to open file runtimelogs.txt");
+            }
+            file << log << std::endl;
+        } catch (const std::exception& e) {
+            warning(e.what(), false);
+            std::cerr << "[ERROR]: " << e.what() << std::endl;
+        }
+
     }
 };
 
